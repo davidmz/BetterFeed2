c6db720c21c135fa5966c0d79c810bdf905b73d4
David Mzareulyan <david@hiero.ru>
2017-03-22 13:18:39 +0300

    Поддержка нового ридмора

--- src/js/modules/no-read-more.js
+++ src/js/modules/no-read-more.js
@@ -3,3 +3,5 @@ import {registerModule} from "./../base/modules";
 const module = registerModule("no-read-more", false);
 
 module.watch("a.read-more", a => a.click());
+
+module.watch(".expand-button > i", a => a.click());
