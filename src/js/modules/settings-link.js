6bc1d9e33cac54da97b8d91e2e45200e8b00a865
David Mzareulyan <david@hiero.ru>
2019-12-29 17:59:32 +0300

    Убираем блок BF на странице настроек

--- src/js/modules/settings-link.js
+++ src/js/modules/settings-link.js
@@ -2,8 +2,6 @@ import {registerModule} from "./../base/modules";
 import h from "../utils/html";
 import bfRoot from "../utils/bf-root";
 import Lightbox from "../utils/lightbox";
-import {authToken} from '../utils/current-user-id.js';
-import escapeHTML from "../utils/escape-html";
 
 const version = process.env.BF2_VERSION;
 
@@ -27,53 +25,8 @@ module.watch(".sidebar", node => {
 
     node.insertBefore(html, null);
 
-    link.addEventListener("click", () => showSettings())
+    link.addEventListener("click", showSettings)
 });
-
-if ("__BetterFeedOnSite" in window) {
-    // BF загружен через сайтовый загрузчик
-    module.watch(".bf2-settings-description", node => {
-        if (!document.body.querySelector(".bf2-green")) {
-            greenBarBefore(node);
-        }
-    });
-} else {
-    // BF загружен как юзерскрипт
-    module.watch(".box-body > form > h3", node => {
-        if (document.body.querySelector(".bf2-green") || node.textContent !== "Change password") {
-            return;
-        }
-        const pwdForm = node.parentNode;
-        const descNode = h("p.bf2-settings-description",
-            "BetterFeed is an add-on that improves or dramatically alters stock interface of FreeFeed (",
-            h("a", {href: "https://github.com/davidmz/BetterFeed2", target: "_blank"}, "details"),
-            "). "
-        );
-        const html = h("div",
-            h("h3.bf2-settings-header", "Add-ons"),
-            descNode,
-            h("hr")
-        );
-        pwdForm.parentNode.insertBefore(html, pwdForm);
-        greenBarBefore(descNode);
-    });
-}
-
-function greenBarBefore(node) {
-    const sLink = h("a", "configure settings");
-    const showTokenLink = h("a", "show access token");
-    node.parentNode.insertBefore(
-        h("p.bg-success.bf2-green",
-            `BetterFeed enabled (${version}) | `,
-            h("i.fa.fa-cog"), " ", sLink,
-            ' | ',
-            h("i.fa.fa-exclamation-triangle"), " ", showTokenLink
-        ),
-        node
-    );
-    sLink.addEventListener("click", () => showSettings());
-    showTokenLink.addEventListener("click", () => showAccessToken());
-}
 
 function showSettings() {
     const url = bfRoot + '/src/html/settings.html?origin=' + encodeURIComponent(location.origin);
@@ -85,33 +38,4 @@ function showSettings() {
 
     new Lightbox("bf2-settings-lightbox")
         .showContent(h("iframe", {src: url, frameborder: "0"}));
-}
-
-function showAccessToken() {
-    const html = `
-        <p>
-            Your access token is:
-        </p>
-        <p class="bf2-access-token">
-            ${escapeHTML(authToken)}
-        </p>
-        <p>
-            <strong>WARNING:</strong> Keep your token secure at all times and do not share it with services you do not fully trust.
-            They will be able to perform ANY operation on your behalf including changing your password and
-            <strong>locking you out of your account</strong>.
-        </p>
-        <p>
-            Giving your token to third party services provides them with FULL and UNLIMITED access to your account.
-            You can not change the token, you cannot revert access once you give it out.
-        </p>
-        <p>
-            If you suspect your token is being abused, contact <a href="mailto:freefeed.net@gmail.com">freefeed.net@gmail.com</a> <strong>immediately</strong>.
-        </p>
-        <p style="text-align: center">
-            <button>Close</button>
-        </p>
-    `;
-    const lb = new Lightbox("bf2-access-token-lightbox");
-    lb.showContent(html);
-    lb.contentEl.querySelector("button").addEventListener("click", () => lb.hide());
 }
