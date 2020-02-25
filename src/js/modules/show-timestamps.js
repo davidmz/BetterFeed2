1c5efdc2f5b23de52ca3502acc60c7cc953d4e39
David Mzareulyan <david@hiero.ru>
2020-02-25 13:54:40 +0300

    Новый класс для иконки приватности поста

--- src/js/modules/show-timestamps.js
+++ src/js/modules/show-timestamps.js
@@ -2,4 +2,4 @@ import {registerModule} from "./../base/modules";
 
 const module = registerModule("show-timestamps", false);
 
-module.watch(".post-timestamps-toggle", t => t.click());
+module.watch(".post-lock-icon", t => t.click());
