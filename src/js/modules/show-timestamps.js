ab3fec0bc9e4a69a149c61e5e42180642a0d18c6
David Mzareulyan <david@hiero.ru>
2020-02-25 14:06:26 +0300

    Правильная эмуляция клика на иконке приватности

--- src/js/modules/show-timestamps.js
+++ src/js/modules/show-timestamps.js
@@ -2,4 +2,8 @@ import {registerModule} from "./../base/modules";
 
 const module = registerModule("show-timestamps", false);
 
-module.watch(".post-lock-icon", t => t.click());
+module.watch(".post-lock-icon", t => {
+    const event = document.createEvent("MouseEvents");
+    event.initEvent("click", true, true);
+    t.dispatchEvent(event);
+});
