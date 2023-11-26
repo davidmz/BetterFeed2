9826d86abef39cf85c6574e00d46649eaf85c352
David Mzareulyan <david@hiero.ru>
2023-11-26 14:43:33 +0300

    Show note under the comment form of non-private posts

--- src/js/modules/nonprivate-warn.js
+++ src/js/modules/nonprivate-warn.js
@@ -1,5 +1,21 @@
+import h from "../utils/html";
 import { registerModule } from "./../base/modules";
 
-registerModule("nonprivate-warn", false).init(() =>
-  document.body.classList.add("bf2-non-private-warn"),
-);
+registerModule("nonprivate-warn", false).watch(".comment-textarea", (node) => {
+  /** @type {HTMLElement} */
+  const commentNode = node.closest(".comment");
+  if (commentNode.nextElementSibling) {
+    return;
+  }
+
+  const postIcon = commentNode
+    .closest(".post-body")
+    .querySelector(".post-footer-icon svg");
+  if (!postIcon || postIcon.classList.contains("post-private-icon")) {
+    return;
+  }
+
+  commentNode.appendChild(
+    h(".bf2-nonprivate-warning", "Note: this post is not private"),
+  );
+});
